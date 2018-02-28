24dc5501061bfe3519f5688b674c32524c5bfe5c
18227754041 <duanzhangtao@gmail.com>
2018-02-28 15:16:35 +0800

    add assets

---
+++ assets/src/helloworld.vue
@@ -0,0 +1,39 @@
+<template>
+  <h1>{{ msg }}</h1>
+</template>
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'helloworld',
+  data () {
+    return {
+      msg: 'Hello World.'
+    }
+  },
+  created () {
+    // 组件创建完后获取数据，
+    // 此时 data 已经被 observed 了
+    this.fetchData()
+  },
+  methods: {
+    fetchData () {
+        var _this = this;
+        axios.post('/civ')
+          .then(function (response) {
+            _this.msg = response.data.a;
+            console.log(response.data.a);
+            console.log(_this);
+          })
+          .catch(function (error) {
+            console.log(error.data);
+          });
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+h1 {
+  color: $green;
+}
+</style>
---
+++ assets/src/user.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>User {{ $route.params.id }} {{msg}}</div>
+</template>
+<script>
+export default {
+  name: 'user',
+  data () {
+    return {
+      msg: 'Hello World.'
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+h1 {
+  color: $green;
+}
+</style>
